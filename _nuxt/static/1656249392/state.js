4a5d7714d5461bf1b5ea59a1553facb8767acebe
Sergii <iakovyshyn.s@gmail.com>
2022-06-26 16:18:39 +0300

    deploy on gh-pages

---
+++ _nuxt/static/1656249392/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftttteeessstttt\u002F_nuxt\u002Fstatic\u002F1656249392",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftttteeessstttt\u002F",assetsPath:"\u002Ftttteeessstttt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
